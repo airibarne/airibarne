0c6ec47f4d7c874d83facd378954ee2330778820
Albert Iribarne <albert.iribarne@gmail.com>
2020-06-05 13:14:57 +0200

    init burger menu component

---
+++ frontend/src/components/BurgerMenu.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="menu-toggle">
+        <span class="menu-toggle--hamburger">
+
+        </span>
+    </button>
+</template>
+
+<script lang="ts">
+    import { Component, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class BurgerMenu extends Vue {
+        public isBurgerActive: boolean = false;
+    }
+</script>
+
+<style scoped>
+
+</style>
