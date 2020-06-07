44e7df7e5d86cd413000a397a82830494ec11adb
Albert Iribarne <albert.iribarne@gmail.com>
2020-06-07 20:38:54 +0200

    add internationalization

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,10 @@ import Vue from 'vue';
 
 import App from '@/App.vue';
 import router from '@/router';
+import i18n from './i18n';
 
 new Vue({
   router,
+  i18n,
   render: (h) => h(App),
 }).$mount('#app');
